35af76b937a5c4d657164e8475c4cb11437022a0
Benzom13 <113477024+Benzom13@users.noreply.github.com>
2022-12-12 20:59:59 -0800

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './polyfills.ts';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+
+if (environment.production) {
+  enableProdMode();
+}
+
